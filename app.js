d6d10cd9b7466f624a955356dc6d059133673300
nattawat1 <bert_nattawat@hotmail.com>
2022-05-01 00:01:32 +0700

    Added front-end

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
     res.send('Hello Nattawat');
