fc552ef96e7f3891821e77c51d35b0e4f5690692
nattawat1 <bert_nattawat@hotmail.com>
2022-05-03 03:44:23 +0700

    Added products

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const req = require('express/lib/request');
 const path = require('path');
 
 const app = express();
 const port = 3000;
+const productRouter = express.Router();
 
 app.use(express.static(path.join(__dirname, "/public/")));
+
+productRouter.route("/").get ( (req, res) => {
+    res.render("products");
+})
+
+productRouter.route("/1").get ( (req, res) => {
+    res.send("Hello World! I'm Product first");
+})
+
+app.use("/products", productRouter)
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
@@ -12,6 +24,7 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render('index', { username: 'Bert'});
 })
+
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
